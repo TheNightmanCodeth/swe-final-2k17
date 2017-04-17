c7181ec1875103b937dcc217f748e50bab4141d8
TheNightman <joyod3@gmail.com>
2017-04-16 23:23:37 -0400

    Adds search form with isbn functionality to index page

--- src/app.py
+++ src/app.py
@@ -1,13 +1,23 @@
 ''' This file is where we define all routes and site-related logic'''
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, redirect
+from BookParser import BookParser
 
 app = Flask(__name__, static_url_path='/static')
 
 #Routes
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    #this method is called when the '/' endpoint receives a GET request
+    if request.method == 'POST':
+        #this method is called when the '/' endpoint receives a POST request
+        results = []
+        form = request.form
+        #TODO: Add search for other keywords
+        bp = BookParser()
+        if form['ISBN'] is not None:
+            results = bp.search_by_isbn(form['ISBN'])
+        return render_template('main.html', results=results)
+
     return render_template('main.html')
 
 if __name__ == "__main__":
