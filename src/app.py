90d58406a17318121782da1e64f12242fd163806
TheNightman <joyod3@gmail.com>
2017-04-16 21:41:22 -0400

    Begins flask implementation

--- src/app.py
+++ src/app.py
@@ -9,3 +9,6 @@ app = Flask(__name__, static_url_path='/static')
 def home():
     #this method is called when the '/' endpoint receives a GET request
     return render_template('main.html')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
